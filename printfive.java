a84c0da1d468915ad9ab4fe75b96fec64b25204d
sankarram24 <sankarram242@gmail.com>
2016-08-02 14:10:23 +0530

    Create printfive.java

---
+++ printfive.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class printfive
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		/*Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int a=s.nextInt();*/
+		int a=5;
+		for(int i=1;i<=a;i++)
+		{
+			System.out.println("HELLO\n");
+		}
+		
+		
+		
+		
+		
+		
+		
+		// your code goes here
+	}
+}
