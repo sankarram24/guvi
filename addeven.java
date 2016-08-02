2117ee97d4a81a52c343dc6bb829fd725c23f0bb
sankarram24 <sankarram242@gmail.com>
2016-08-02 13:53:06 +0530

    Create addeven.java

---
+++ addeven.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class addeven
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number\n");
+		int n=s.nextInt();
+		if(n%2==0)
+		{
+			System.out.println("the number is even");
+		}
+		else
+		{
+			System.out.println("the number is odd");
+		}// your code goes here
+	}
+}
