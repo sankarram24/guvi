d3741b3915d83ddaf93f185ddb97b68e25fa857b
sankarram24 <sankarram242@gmail.com>
2016-08-02 12:41:42 +0530

    Update pos_neg.java

--- pos_neg.java
+++ pos_neg.java
@@ -1,30 +1,26 @@
-* package whatever; // don't place package name! */
+/* package whatever; // don't place package name! */
 
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+import java.io.DataInputStream;
 
 /* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	
-		Scanner read=new Scanner(System.in);
+		
+		Scanner s=new Scanner(System.in);
 		System.out.println("Enter the number\n");
-		int a=read.nextInt();
-		if(a==0)
+		int n=s.nextInt();
+		if(n%2==0)
 		{
-			System.out.println("the number is zero");
-		}
-		if(a>0)
-		{
-			System.out.println("the number is positive");
+			System.out.println("the number is even");
 		}
 		else
 		{
-			System.out.println("the number is negative");
+			System.out.println("the number is odd");
 		}
-		// your code goes here
 	}
 }
