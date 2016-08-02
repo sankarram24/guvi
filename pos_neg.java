d7b84f2bdf08cbf14150c52b77189de952b0f9b8
sankarram24 <sankarram242@gmail.com>
2016-08-02 12:13:55 +0530

    Update pos_neg.java

--- pos_neg.java
+++ pos_neg.java
@@ -1,1 +1,30 @@
-szdsdgdfg
+* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+		Scanner read=new Scanner(System.in);
+		System.out.println("Enter the number\n");
+		int a=read.nextInt();
+		if(a==0)
+		{
+			System.out.println("the number is zero");
+		}
+		if(a>0)
+		{
+			System.out.println("the number is positive");
+		}
+		else
+		{
+			System.out.println("the number is negative");
+		}
+		// your code goes here
+	}
+}
