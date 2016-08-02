85c875906a99ee152029a564d8b27215422644b8
sankarram24 <sankarram242@gmail.com>
2016-08-02 16:57:13 +0530

    Create printeven.java

---
+++ printeven.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class printeven
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner s= new Scanner(System.in);
+		// your code goes here
+		System.out.println("Enter the intervals\n");
+		int a=s.nextInt();
+		int b=s.nextInt();
+		for(int i=a;i<=b;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i+"");
+			}	// your code goes here
+		}
+	}
+}
