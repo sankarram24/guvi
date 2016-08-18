aca09bede070514d63d163cd1ed82336526aac47
sankarram24 <sankarram242@gmail.com>
2016-08-18 12:09:19 +0530

    Create pointeradd.c

---
+++ pointeradd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    //printf("Hello, World!\n");
+int *ptr1;
+int *ptr2;
+int *ptr3;
+scanf("%d",ptr1);
+scanf("%d",ptr2);
+*ptr3=*ptr1+*ptr2;
+printf("%d",*ptr3);
+    return 0;
+}
