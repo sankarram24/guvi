85109b97c288ecece407b9196bf1c9053d40acb5
sankarram24 <sankarram242@gmail.com>
2016-08-24 17:06:38 +0530

    Create decimaltobinary.java

---
+++ decimaltobinary.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class decimaltobinary
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		//int b=
+		
+		System.out.println(Integer.toBinaryString(a));
+	}
+}
